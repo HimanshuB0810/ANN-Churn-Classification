8be3df63182350a2c07c5c698cfa51821c9e7cb8
HimanshuB0810 <himanshuborikar10@gmail.com>
2025-09-22 22:29:13 +0530

    Different Scaler and Ecoder created

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -9,13 +9,13 @@ import pickle
 model=tf.keras.models.load_model("regression_model.h5")
 
 # Load the encoders and scaler
-with open("label_encoder_gender.pkl","rb") as file:
+with open("regression_label_encoder_gender.pkl","rb") as file:
     label_encoder_gender=pickle.load(file)
 
-with open("onehot_encoder_geo.pkl","rb") as file:
+with open("regression_onehot_encoder_geo.pkl","rb") as file:
     onehot_encoder_geo=pickle.load(file)
 
-with open("scaler.pkl","rb") as file:
+with open("regression_scaler.pkl","rb") as file:
     scaler=pickle.load(file)
 
 ## Streamlit app
